9be47dfb2d1a8b7581736267a09f5789a89a9a83
Pavel Gyurginchev <gyurginchev@windowslive.com>
2018-12-19 20:52:00 +0200

    Added BaseService as repository

---
+++ src/NiceShop.Data.Services/BaseService.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NiceShop.Data.Models;
+
+namespace NiceShop.Data.Services
+{
+    public class BaseService<TEntity>
+        where TEntity : BaseModel
+    {
+        private readonly DbSet<TEntity> dbSet;
+
+        protected NiceShopDbContext Db { get; }
+
+        public BaseService(NiceShopDbContext db)
+        {
+            this.Db = db;
+            this.dbSet = this.Db.Set<TEntity>();
+        }
+
+        public TEntity GetById(string id)
+        {
+            return this.dbSet.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return this.dbSet;
+        }
+
+        public void Add(TEntity entity)
+        {
+            this.dbSet.Add(entity);
+            this.Db.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            this.dbSet.Remove(entity);
+            this.Db.SaveChanges();
+        }
+
+        public void Edit(TEntity entity)
+        {
+            this.Db.Entry(entity).State = EntityState.Modified;
+            this.Db.SaveChanges();
+        }
+    }
+}
