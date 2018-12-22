601354bec04099e85e15f85ab741ac87e7f06993
Pavel Gyurginchev <gyurginchev@windowslive.com>
2018-12-22 23:29:42 +0200

    Small changes to view models

--- src/NiceShop.ViewModels/IdAndNameViewModel.cs
+++ src/NiceShop.ViewModels/IdAndNameViewModel.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace NiceShop.ViewModels
+﻿namespace NiceShop.ViewModels
 {
     public class IdAndNameViewModel
     {
         public string Id { get; set; }
 
-        [Display(Name = "Име")]
-        [Required(ErrorMessage = "{0} е задължително поле!")]
         public string Name { get; set; }
     }
 }
