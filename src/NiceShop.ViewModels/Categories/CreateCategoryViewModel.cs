18b525d547371e36142ea9ec70109495f4afde09
Pavel Gyurginchev <gyurginchev@windowslive.com>
2018-12-22 15:40:38 +0200

    Added create category

---
+++ src/NiceShop.ViewModels/Categories/CreateCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NiceShop.ViewModels.Categories
+{
+    public class CreateCategoryViewModel
+    {
+        [Required(ErrorMessage = "Полето {0} е задължително!")]
+        [Display(Name = "Име")]
+        public string Name { get; set; }
+    }
+}
--- src/NiceShop.Web/Areas/Administration/Controllers/CategoriesController.cs
+++ src/NiceShop.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
-﻿using NiceShop.Data.Services.Administration.Contracts;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NiceShop.Data.Services.Administration.Contracts;
+using NiceShop.ViewModels.Categories;
 
 namespace NiceShop.Web.Areas.Administration.Controllers
 {
@@ -9,6 +13,30 @@ namespace NiceShop.Web.Areas.Administration.Controllers
         public CategoriesController(ICategoriesService categoriesService)
         {
             this.categoriesService = categoriesService;
+        }
+
+        public IActionResult Create()
+        {
+            var categories = this.categoriesService
+                .GetAll()
+                .ToList();
+
+            this.ViewData["categories"] = categories;
+
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryViewModel viewModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                // TODO: Do something
+            }
+
+            await this.categoriesService.CreateAsync(viewModel.Name);
+
+            return this.RedirectToAction("Create");
         }
     }
 }
